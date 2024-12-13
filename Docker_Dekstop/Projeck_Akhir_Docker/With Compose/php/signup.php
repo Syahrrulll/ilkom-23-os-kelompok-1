550b56a047d2c9d65fa0710eb9a79adfd4059a30
Syahrrulll <Syahrulmubarak3225@gmail.com>
2024-12-13 11:03:53 +0800

    perbaiki

---
+++ Docker_Dekstop/Projeck_Akhir_Docker/With Compose/php/signup.php
@@ -0,0 +1,91 @@
+<?php
+
+    include 'service/database.php';
+
+        
+    if(isset($_POST['signup'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $sql = "SELECT * FROM users WHERE 
+        username='$username' AND password='$password' ";
+        $result = $db->query($sql);
+        if($result->num_rows >= 1){
+            echo '<script> alert("Akun Sudah Ada!!")</script>';
+        }else{
+            $sql = "INSERT INTO users (username, password) VAlUES
+            ('$username', '$password')";
+
+            if($db->query($sql)){
+                echo '<script> alert("Daftar Akun Berhasil, Silahkan Login")</script>';
+            }else{
+
+                echo '<script> alert("Daftar Akun Gagal, Coba Lagi")</script>';
+            }
+        }
+
+    }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style_sign.css">
+    <title>Sign Up</title>
+</head>
+<body>
+<div class="box-login">
+    </div>
+    <div class="backgroun-sign" style ="
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-image: url('image/Background_login.jpg');
+    background-size: cover;
+    background-position: center;
+    opacity: 0.2;
+    z-index: -1;
+    filter : grayscale(1);
+    "></div>
+    <div class="border-sign">
+    </div>
+    <div class="sign">
+        SignUp
+    </div>
+
+    <form action="signup.php" method="POST">
+        <div class="input-username">
+            <input type="text" placeholder="Username" name="username">
+        </div>
+            
+        <div class="border-password">
+            <input type='password' placeholder='Password' name='password'> 
+        </div>
+
+        <div class="kirim">
+            <button type="submit" name="signup">Daftar</button>
+        </div>
+    </form>
+        <div class="caption-signin">
+            Sudah punya akun? silakan
+        </div>
+        <div class="daftar">
+            <a href="signin.php" name="signup">masuk</a>
+        </div>
+
+        <div class="lewati">
+            Lewati
+        </div>
+        <div class='skip'>
+            <a href="../">
+                <img src ="image/lewati.png">
+            </a>
+        </div>
+</body>
+</html>
